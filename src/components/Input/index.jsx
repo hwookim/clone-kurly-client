2f2126f878ac6572cb3a9fd6330901f745d7a2ba
hwookim <gusdn9754@gmail.com>
2022-08-26 22:00:38 +0900

    feat: Input 내 경고 문구 추가

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.scss';
 
-export default function Input({ id, name, placeholder = '', type = 'text' }) {
+export default function Input({ id, name, placeholder = '', type = 'text', warning = '' }) {
   return (
     <div className="common-input">
       <input id={id} name={name} placeholder={placeholder} type={type}></input>
+      {warning && <div className="warning">{warning}</div>}
     </div>
   );
 }
--- src/components/Input/index.stories.jsx
+++ src/components/Input/index.stories.jsx
@@ -19,3 +19,9 @@ Password.args = {
   placeholder: '비밀번호를 입력해주세요',
   type: 'password',
 };
+
+export const WithWarning = Template.bind({});
+WithWarning.args = {
+  placeholder: '아이디를 입력해주세요',
+  warning: '6자 이상 16자 이하의 영문 혹은 영문과 숫자를 조합'
+}
