56bab00160f0933b58b821d3daecd1119c3ae29a
hwookim <gusdn9754@gmail.com>
2022-08-31 23:48:12 +0900

    feat: promotions 데이터 호출 구현

--- src/pages/Main/Banner/Banner.stories.jsx
+++ src/pages/Main/Banner/Banner.stories.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Banner from './index';
+import { rest } from 'msw';
+import PROMOTIONS from '../../../../__mock__/promotions.json';
 
 export default {
   title: 'common/Banner',
@@ -11,3 +13,12 @@ const Template = (args) => <Banner {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get('/promotions', (req, res, ctx) => {
+        return res(ctx.json(PROMOTIONS));
+      }),
+    ],
+  },
+};
--- src/pages/Main/Banner/index.jsx
+++ src/pages/Main/Banner/index.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.scss';
+import api from '../../../utils/api';
+import { Link } from 'react-router-dom';
 
 export default function Banner() {
+  const [dataset, setDataset] = useState(null);
+
+  useEffect(() => {
+    api.get('/promotions').then((data) => setDataset(data));
+  }, []);
+
   return (
     <div className="banner">
-      <div className="banner__image-container"></div>
+      <div className="banner__image-container">
+        {dataset?.map(({ id, image, link }) => (
+          <Link key={id} to={link} className="banner__image-container__link">
+            <img src={image} alt={`banner-${id}`} className="banner__image-container__link__image" />
+          </Link>
+        ))}
+      </div>
       <button className="banner__btn banner__btn-left">
         <span className="material-symbols-outlined">chevron_left</span>
       </button>
