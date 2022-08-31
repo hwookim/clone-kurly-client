26452ff55dd1cbbbb25f115214c0d68b2450f4f6
hwookim <gusdn9754@gmail.com>
2022-08-31 23:55:26 +0900

    feat: 버튼에 의한 배너 이동 구현

--- src/pages/Main/Banner/index.jsx
+++ src/pages/Main/Banner/index.jsx
@@ -1,31 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './Banner.scss';
 import api from '../../../utils/api';
 import { Link } from 'react-router-dom';
 
 export default function Banner() {
-  const [dataset, setDataset] = useState(null);
+  const [dataset, setDataset] = useState([]);
+  const [current, setCurrent] = useState(0);
+  const bannerRef = useRef(null);
 
   useEffect(() => {
     api.get('/promotions').then((data) => setDataset(data));
   }, []);
 
+  useEffect(() => {
+    if (!bannerRef?.current) return;
+
+    bannerRef.current.style.transition = 'all 0.5s ease-in-out';
+    bannerRef.current.style.transform = `translateX(-${current}00%)`;
+  }, [current]);
+
+  const moveCarousel = useCallback(
+    (direction) => {
+      const value = current + direction;
+      if (value < 0) {
+        setCurrent(dataset.length - 1);
+        return;
+      }
+      if (value >= dataset.length) {
+        setCurrent(0);
+        return;
+      }
+      setCurrent(value);
+    },
+    [current, dataset.length]
+  );
+
+  const onClickLeftButton = () => {
+    moveCarousel(-1);
+  };
+
+  const onClickRightButton = () => {
+    moveCarousel(+1);
+  };
+
   return (
     <div className="banner">
-      <div className="banner__image-container">
-        {dataset?.map(({ id, image, link }) => (
+      <div ref={bannerRef} className="banner__image-container">
+        {dataset.map(({ id, image, link }) => (
           <Link key={id} to={link} className="banner__image-container__link">
             <img src={image} alt={`banner-${id}`} className="banner__image-container__link__image" />
           </Link>
         ))}
       </div>
-      <button className="banner__btn banner__btn-left">
+      <button className="banner__btn banner__btn-left" onClick={onClickLeftButton}>
         <span className="material-symbols-outlined">chevron_left</span>
       </button>
-      <button className="banner__btn banner__btn-right">
+      <button className="banner__btn banner__btn-right" onClick={onClickRightButton}>
         <span className="material-symbols-outlined">chevron_right</span>
       </button>
-      <div className="banner__counter">0 / 0</div>
+      <div className="banner__counter">
+        {current + 1} / {dataset.length}
+      </div>
     </div>
   );
 }
