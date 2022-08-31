5675c8db184cfeaec1267e566774869ddd7073cc
hwookim <gusdn9754@gmail.com>
2022-08-31 19:03:27 +0900

    feat: 세부 카테고리를 제외한 카테고리 목록 표시 구현

--- src/components/Header/Category.jsx
+++ src/components/Header/Category.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Category.scss';
+import api from '../../utils/api';
 
 export default function Category() {
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    api.get('/categories').then((data) => setCategories(data));
+  }, []);
+
   return (
     <div className="category">
-      <span className="category__icon material-symbols-outlined">menu</span>카테고리
+      <span className="category__icon material-symbols-outlined">menu</span>
+      <span className="category__text">카테고리</span>
+      <div className="category__dropdown">
+        {categories?.map(({ id, name }) => (
+          <div key={name + id} className="category__dropdown__item">
+            {name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
