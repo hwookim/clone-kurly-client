0e47070a928be7783c6d336c8e1ba1c2f98b4d33
hwookim <gusdn9754@gmail.com>
2022-09-02 10:36:57 +0900

    feat: 메인 페이지 상품 목록 Carousel 적용

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -5,13 +5,23 @@ import Banner from './Banner';
 import MainProductList from './MainProductList';
 
 import api from '../../utils/api';
+import useCarousel from '../../hooks/useCarousel';
 
 export default function MainPage() {
   const [products, setProducts] = useState([]);
+  const { ref: carouselRef, moveCarousel } = useCarousel({ length: products.length / 4 });
 
   useEffect(() => {
     api.get('/products').then((data) => setProducts(data));
   }, []);
+
+  const onClickLeftButton = () => {
+    moveCarousel(-1);
+  };
+
+  const onClickRightButton = () => {
+    moveCarousel(+1);
+  };
 
   return (
     <>
@@ -20,13 +30,21 @@ export default function MainPage() {
       <section className="main-contents">
         <div className="main-contents__title">이 상품 어때요?</div>
         <div className="main-contents__carousel">
-          <div className="main-contents__carousel__product-list">
-            <MainProductList products={products} />
+          <div className="main-contents__carousel__content">
+            <div className="main-contents__carousel__content__product-list" ref={carouselRef}>
+              <MainProductList products={products} />
+            </div>
           </div>
-          <button className="main-contents__carousel__btn main-contents__carousel__btn-left">
+          <button
+            className="main-contents__carousel__btn main-contents__carousel__btn-left"
+            onClick={onClickLeftButton}
+          >
             <span className="material-symbols-outlined">arrow_back_ios</span>
           </button>
-          <button className="main-contents__carousel__btn main-contents__carousel__btn-right">
+          <button
+            className="main-contents__carousel__btn main-contents__carousel__btn-right"
+            onClick={onClickRightButton}
+          >
             <span className="material-symbols-outlined">arrow_forward_ios</span>
           </button>
         </div>
