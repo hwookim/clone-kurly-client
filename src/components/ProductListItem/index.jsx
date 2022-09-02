711e22e11101bd8b221114cf2dd7272ab75b6b4b
hwookim <gusdn9754@gmail.com>
2022-09-02 14:41:37 +0900

    feat: 장바구니 버튼 클릭 시 링크 이동을 막도록 변경
    링크에서 onClick을 통한 이동으로 변경

--- src/components/ProductListItem/index.jsx
+++ src/components/ProductListItem/index.jsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useMemo } from 'react';
 import './ProductListItem.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductListItem({ product }) {
   const { id, title, thumbnail, price, discount } = product;
@@ -9,25 +9,37 @@ export default function ProductListItem({ product }) {
     () => (discount ? (parseInt(price) * (1 - parseFloat(discount))).toLocaleString('ko-KR') : originPrice),
     [discount, originPrice, price]
   );
+  const navigate = useNavigate();
+
+  const onClickProduct = () => {
+    navigate(`/products/${id}`);
+  };
+
+  const onClickCartButton = useCallback((event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  }, []);
 
   return (
     <div className="product-list-item">
-      <Link key={title + id} to={`/products/${id}`}>
-        <div className="product-list-item__thumbnail">
-          <img src={thumbnail} alt="title" className="product-list-item__thumbnail__img" />
-          <button className="product-list-item__thumbnail__btn">
-            <span className="product-list-item__thumbnail__btn__icon material-symbols-outlined">shopping_cart</span>
+      <div className="product-list-item__content" onClick={onClickProduct}>
+        <div className="product-list-item__content__thumbnail">
+          <img src={thumbnail} alt="title" className="product-list-item__content__thumbnail__img" />
+          <button className="product-list-item__content__thumbnail__btn" onClick={onClickCartButton}>
+            <span className="product-list-item__content__thumbnail__btn__icon material-symbols-outlined">
+              shopping_cart
+            </span>
           </button>
         </div>
-        <div className="product-list-item__info">
-          <h3 className="product-list-item__info__title">{title}</h3>
-          <div className="product-list-item__info__price">
-            {discount && <span className="product-list-item__info__price__discount">{discount * 100}%</span>}
-            <span className="product-list-item__info__price__sales">{salesPrice}원</span>
-            {discount && <div className="product-list-item__info__price__origin">{originPrice}원</div>}
+        <div className="product-list-item__content__info">
+          <h3 className="product-list-item__content__info__title">{title}</h3>
+          <div className="product-list-item__content__info__price">
+            {discount && <span className="product-list-item__content__info__price__discount">{discount * 100}%</span>}
+            <span className="product-list-item__content__info__price__sales">{salesPrice}원</span>
+            {discount && <div className="product-list-item__content__info__price__origin">{originPrice}원</div>}
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
