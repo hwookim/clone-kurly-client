4da0c5fc462cd3e33a4c5644e9e15806d1c65a7e
hwookim <gusdn9754@gmail.com>
2022-09-02 18:26:05 +0900

    feat: 입력값 검증 구현

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignupPage.scss';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import rules from '../../utils/rules';
 
 export default function SignupPage() {
+  const [password, setPassword] = useState('');
+  const [messages, setMessages] = useState({
+    id: '',
+    password: '',
+    passwordCheck: '',
+    name: '',
+  });
+
+  const onChange = (event) => {
+    const { id, value } = event.target;
+
+    if (id === 'password') {
+      setPassword(value);
+    }
+
+    rules.signup[id].forEach((rule) => {
+      if (rule.validate(value, password)) {
+        setMessages((prev) => ({
+          ...prev,
+          [id]: '',
+        }));
+        return;
+      }
+      setMessages((prev) => ({
+        ...prev,
+        [id]: rule.message,
+      }));
+    });
+  };
+
   const onSubmit = (event) => {
     event.preventDefault();
   };
@@ -16,30 +47,30 @@ export default function SignupPage() {
           <span className="point">* </span>
           필수입력사항
         </div>
-        <form className="signup__content__form" onSubmit={onSubmit}>
+        <form className="signup__content__form" onChange={onChange} onSubmit={onSubmit}>
           <div className="signup__content__form__input">
             <label htmlFor="id">
               아이디<span className="point">*</span>
             </label>
-            <Input id="id" placeholder="아이디를 입력해주세요" />
+            <Input id="id" placeholder="아이디를 입력해주세요" warning={messages.id} />
           </div>
           <div className="signup__content__form__input">
             <label htmlFor="password">
               비밀번호<span className="point">*</span>
             </label>
-            <Input id="password" placeholder="비밀번호를 입력해주세요" />
+            <Input id="password" placeholder="비밀번호를 입력해주세요" warning={messages.password} />
           </div>
           <div className="signup__content__form__input">
-            <label htmlFor="password-check">
+            <label htmlFor="passwordCheck">
               비밀번호 확인<span className="point">*</span>
             </label>
-            <Input id="password-check" placeholder="비밀번호를 한번 더 입력해주세요" />
+            <Input id="passwordCheck" placeholder="비밀번호를 한번 더 입력해주세요" warning={messages.passwordCheck} />
           </div>
           <div className="signup__content__form__input">
             <label htmlFor="name">
               이름<span className="point">*</span>
             </label>
-            <Input id="name" placeholder="이름을 입력해주세요" />
+            <Input id="name" placeholder="이름을 입력해주세요" warning={messages.name} />
           </div>
           <Button type="submit" variant="primary">
             가입하기
