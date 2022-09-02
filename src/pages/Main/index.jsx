285bb36bee0cbe7f77cfa09b4aa03daf010edc1d
hwookim <gusdn9754@gmail.com>
2022-09-02 10:41:56 +0900

    feat: 양 끝에서 버튼 사라지도록 구현

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -6,10 +6,12 @@ import MainProductList from './MainProductList';
 
 import api from '../../utils/api';
 import useCarousel from '../../hooks/useCarousel';
+import { useMemo } from 'react';
 
 export default function MainPage() {
   const [products, setProducts] = useState([]);
-  const { ref: carouselRef, moveCarousel } = useCarousel({ length: products.length / 4 });
+  const carouselLength = useMemo(() => products.length / 4, [products]);
+  const { ref: carouselRef, current, moveCarousel } = useCarousel({ length: carouselLength });
 
   useEffect(() => {
     api.get('/products').then((data) => setProducts(data));
@@ -35,18 +37,22 @@ export default function MainPage() {
               <MainProductList products={products} />
             </div>
           </div>
-          <button
-            className="main-contents__carousel__btn main-contents__carousel__btn-left"
-            onClick={onClickLeftButton}
-          >
-            <span className="material-symbols-outlined">arrow_back_ios</span>
-          </button>
-          <button
-            className="main-contents__carousel__btn main-contents__carousel__btn-right"
-            onClick={onClickRightButton}
-          >
-            <span className="material-symbols-outlined">arrow_forward_ios</span>
-          </button>
+          {current === 0 || (
+            <button
+              className="main-contents__carousel__btn main-contents__carousel__btn-left"
+              onClick={onClickLeftButton}
+            >
+              <span className="material-symbols-outlined">arrow_back_ios</span>
+            </button>
+          )}
+          {current === carouselLength - 1 || (
+            <button
+              className="main-contents__carousel__btn main-contents__carousel__btn-right"
+              onClick={onClickRightButton}
+            >
+              <span className="material-symbols-outlined">arrow_forward_ios</span>
+            </button>
+          )}
         </div>
       </section>
     </>
