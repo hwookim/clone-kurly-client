b8fb64f42ccfbcfb469587b150416130ec71f487
hwookim <gusdn9754@gmail.com>
2022-09-03 15:08:39 +0900

    refactor: 버튼 코드 리팩토링

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,6 @@
-import React, { useMemo } from 'react';
+import React  from 'react';
 import './Button.scss';
 
 export default function Button({ variant = 'default', ...args }) {
-  const className = useMemo(() => (variant === 'primary' ? 'primary' : 'default'), [variant]);
-
-  return <button className={"btn " + className} {...args} />;
+  return <button className={"btn " + variant} {...args} />;
 }
