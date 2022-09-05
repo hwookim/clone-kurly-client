a924f8f1545cd48340d6666a121e16f00a633db3
hwookim <gusdn9754@gmail.com>
2022-09-05 15:01:33 +0900

    feat: 카테고리 이름 표시

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -7,17 +7,25 @@ import ProductListItem from '../../components/ProductListItem';
 export default function ProductsPage() {
   const [searchParams] = useSearchParams();
   const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState('카테고리');
 
   useEffect(() => {
     api.get(`/products?${searchParams.toString()}`).then((data) => setProducts(data));
   }, [searchParams]);
+  useEffect(() => {
+    api.get(`/categories/${searchParams.get('category')}`).then((data) => setCategory(data));
+  });
+
   return (
     <div className="products">
-      {products.map((product) => (
-        <div className="products__item">
-          <ProductListItem key={product.id} product={product} />
-        </div>
-      ))}
+      <h3 className="products__title">{category.name}</h3>
+      <div className="products__list">
+        {products.map((product) => (
+          <div className="products__list__item">
+            <ProductListItem key={product.id} product={product} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
