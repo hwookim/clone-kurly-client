f7e87b0a8e55d58efaf8ff57b91cf2387a1bb985
hwookim <gusdn9754@gmail.com>
2022-09-06 23:20:00 +0900

    feat: localstorage 유틸 구현

---
+++ src/utils/localstorage.js
@@ -0,0 +1,20 @@
+const localstorage = {
+  set(key, value) {
+    const localStorageData = window.localStorage.getItem(key);
+    const storageArr = localStorageData ? JSON.parse(localStorageData) : [];
+    storageArr.unshift(value);
+
+    window.localStorage.setItem(key, JSON.stringify(storageArr));
+  },
+
+  get(key) {
+    const localStorageData = window.localStorage.getItem(key);
+    return localStorageData ? JSON.parse(localStorageData) : null;
+  },
+
+  remove(key) {
+    window.localStorage.removeItem(key);
+  },
+};
+
+export default localstorage;
