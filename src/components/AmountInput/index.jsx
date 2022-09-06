2855b0ef9586b5c81b05064f6540b29a1303a92c
hwookim <gusdn9754@gmail.com>
2022-09-06 15:00:24 +0900

    refactor: AmountInput 분리

---
+++ src/components/AmountInput/index.jsx
@@ -0,0 +1,21 @@
+import React  from 'react';
+import './AmountInput.scss'
+
+export default function AmountInput({ value, onChange }) {
+  const onClickAmountButton = (change) => () => {
+    if (value + change <= 0) {
+      return;
+    }
+    onChange(value + change);
+  };
+
+  return (
+    <div className="amount-input">
+      <button onClick={onClickAmountButton(-1)} disabled={value === 1}>
+        -
+      </button>
+      {value}
+      <button onClick={onClickAmountButton(+1)}>+</button>
+    </div>
+  );
+}
