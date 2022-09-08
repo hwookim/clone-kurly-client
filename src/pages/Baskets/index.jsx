fe3786f9d9211412a8e0829ce7f6ad6a1a193a44
hwookim <gusdn9754@gmail.com>
2022-09-08 15:19:15 +0900

    fix: 반복되는 컴포넌트의 unique key 오류 해결

--- src/pages/Baskets/index.jsx
+++ src/pages/Baskets/index.jsx
@@ -121,9 +121,9 @@ export default function BasketsPage() {
                 장바구니에 담긴 상품이 없습니다.
               </li>
             ) : (
-              baskets.map((basket) => (
+              baskets.map((basket, index) => (
                 <BasketItem
-                  key={basket.id}
+                  key={(basket.id, index)}
                   basket={basket}
                   check={selected.includes(basket.id)}
                   onChangeAmount={handleAmountInput}
