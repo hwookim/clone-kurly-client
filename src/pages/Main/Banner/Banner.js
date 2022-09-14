2a8ac81533793d2a9d8a49090990b55e1b6e176f
hwookim <gusdn9754@gmail.com>
2022-09-14 16:10:35 +0900

    refactor: 배너 css BEM 제거

--- src/pages/Main/Banner/Banner.js
+++ src/pages/Main/Banner/Banner.js
@@ -50,37 +50,23 @@ export default function Banner() {
     <div className="banner">
       <div
         ref={bannerRef}
-        className="banner__image-container"
+        className="image-container"
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
         {promotions.map(({ id, img_src, link }, index) => (
-          <Link
-            key={id + '' + index}
-            to={link}
-            className="banner__image-container__link"
-          >
-            <img
-              src={img_src}
-              alt={`banner-${id}`}
-              className="banner__image-container__link__image"
-            />
+          <Link key={id + '' + index} to={link} className="img-wrapper">
+            <img src={img_src} alt={`banner-${id}`} className="img" />
           </Link>
         ))}
       </div>
-      <button
-        className="banner__btn banner__btn-left"
-        onClick={handleClickLeftButton}
-      >
+      <button className="btn btn-left" onClick={handleClickLeftButton}>
         <span className="material-symbols-outlined">chevron_left</span>
       </button>
-      <button
-        className="banner__btn banner__btn-right"
-        onClick={handleClickRightButton}
-      >
+      <button className="btn btn-right" onClick={handleClickRightButton}>
         <span className="material-symbols-outlined">chevron_right</span>
       </button>
-      <div className="banner__counter">
+      <div className="counter">
         {current + 1} / {promotions.length - 2}
       </div>
     </div>
