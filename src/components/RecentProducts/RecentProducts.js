8cc497c1092061f33a69aab4276401920bed6f31
hwookim <gusdn9754@gmail.com>
2022-09-16 14:55:05 +0900

    feat: 최근 목록 carousel 기능 구현

--- src/components/RecentProducts/RecentProducts.js
+++ src/components/RecentProducts/RecentProducts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import localstorage from '../../utils/localstorage';
@@ -7,18 +7,48 @@ import './RecentProducts.scss';
 
 export default function RecentProducts() {
   const products = localstorage.getRecentProducts();
+  const [listTop, setListTop] = useState(0);
+  const isTop = listTop >= 0;
+  const isBottom = listTop < (products.length - 3) * -80;
+
+  const onClickUpBtn = () => {
+    if (isTop) return;
+
+    const target = listTop + 80;
+    if (target > 0) {
+      setListTop(0);
+      return;
+    }
+    setListTop(target);
+  };
+
+  const onClickDownBtn = () => {
+    if (isBottom) return;
+
+    const target = listTop - 80;
+    if (target <= (products.length - 2) * -80) {
+      setListTop((prev) => prev - 44);
+      return;
+    }
+    setListTop(target);
+  };
 
   if (products.length === 0) return <></>;
 
   return (
     <nav className="recent-products">
-      <span className="icon material-symbols-outlined">keyboard_arrow_up</span>
+      <span
+        className={'icon material-symbols-outlined ' + (isTop ? '' : 'active')}
+        onClick={onClickUpBtn}
+      >
+        keyboard_arrow_up
+      </span>
       <p className="recent-text">최근 본 상품</p>
       <div className="recent-list-wrapper">
-        <ul className="recent-list">
+        <ul className="recent-list" style={{ top: `${listTop}px` }}>
           {products.map(({ id, thumbnail }) => (
-            <li key={id}>
-              <Link to={`/products/${id}`} className="recent-list-item">
+            <li key={id} className="recent-list-item">
+              <Link to={`/products/${id}`} className="recent-list-link">
                 <img
                   src={thumbnail}
                   alt={`/products/${id}`}
@@ -29,7 +59,12 @@ export default function RecentProducts() {
           ))}
         </ul>
       </div>
-      <span className="icon material-symbols-outlined">
+      <span
+        className={
+          'icon material-symbols-outlined ' + (isBottom ? '' : 'active')
+        }
+        onClick={onClickDownBtn}
+      >
         keyboard_arrow_down
       </span>
     </nav>
