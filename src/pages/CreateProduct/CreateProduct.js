9f39ad8c8db95bd37e7f7e6710b822f2ebe413cf
hwookim <gusdn9754@gmail.com>
2022-09-20 15:54:36 +0900

    feat: 상품 생성 페이지 기본 레이아웃 구성

--- src/pages/CreateProduct/CreateProduct.js
+++ src/pages/CreateProduct/CreateProduct.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react';
 
+import Input from '../../components/Input';
+
 import './CreateProduct.scss';
 
 export default function CreateProduct() {
@@ -22,26 +24,57 @@ export default function CreateProduct() {
 
   return (
     <div className="create-product">
-      <button
-        className="thumbnail-input-btn"
-        onClick={handleClickThumbnailInput}
-      >
-        {thumbnail ? (
-          <img src={thumbnail} alt="thumbnail-img" className="thumbnail" />
-        ) : (
-          <div className="thumbnail-text">
-            <div class="material-symbols-outlined">image</div>
-            <div>이미지를 선택해주세요</div>
-          </div>
-        )}
-      </button>
-      <input
-        type="file"
-        accept="image/*"
-        hidden
-        ref={imgInputRef}
-        onChange={handleSelectThumbnail}
-      />
+      <div>
+        <button
+          className="thumbnail-input-btn"
+          onClick={handleClickThumbnailInput}
+        >
+          {thumbnail ? (
+            <img src={thumbnail} alt="thumbnail-img" className="thumbnail" />
+          ) : (
+            <div className="thumbnail-text">
+              <div class="material-symbols-outlined">image</div>
+              <div>이미지를 선택해주세요</div>
+            </div>
+          )}
+        </button>
+        <input
+          type="file"
+          accept="image/*"
+          hidden
+          ref={imgInputRef}
+          onChange={handleSelectThumbnail}
+        />
+      </div>
+      <div className="right">
+        <div className="create-input-wrapper">
+          <label className="create-input-label">
+            상품명<span className="point">*</span>
+          </label>
+          <Input className="create-input" />
+        </div>
+        <div className="create-input-wrapper">
+          <label className="create-input-label">상품 설명</label>
+          <Input className="create-input" />
+        </div>
+        <div className="create-input-wrapper">
+          <label className="create-input-label">
+            상품 가격<span className="point">*</span>
+          </label>
+          <Input className="create-input" />
+        </div>
+        <div className="create-input-wrapper">
+          <label className="create-input-label">할인율</label>
+          <Input className="create-input" />
+        </div>
+        <div className="sales-price">
+          할인 후 상품 금액 :<span className="sales-price-value">{0}</span>
+          <span className="sales-price-unit">원</span>
+        </div>
+        <button type="submit" variant="primary" className="submit-btn">
+          상품 생성하기
+        </button>
+      </div>
     </div>
   );
 }
