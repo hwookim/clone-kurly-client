031a6844581421944720c870cdfac387f19b1bdf
hwookim <gusdn9754@gmail.com>
2022-09-20 10:11:01 +0900

    feat: 카카오 맵 마커 이용

--- src/components/KakaoMap/KakaoMap.js
+++ src/components/KakaoMap/KakaoMap.js
@@ -11,11 +11,27 @@ export default function KakaoMap() {
     if (!mapRef.current) return;
 
     const options = {
-      center: new kakao.maps.LatLng(37.5063252948439, 127.0536146558608),
+      center: new kakao.maps.LatLng(COMPANY_POSITION.LAT, COMPANY_POSITION.LNG),
       level: 2,
     };
     setMap(new kakao.maps.Map(mapRef.current, options));
   }, [mapRef]);
 
+  useEffect(() => {
+    const markerPosition = new kakao.maps.LatLng(
+      COMPANY_POSITION.LAT,
+      COMPANY_POSITION.LNG
+    );
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    marker.setMap(map);
+  });
+
   return <div className="kakao-map" ref={mapRef} />;
 }
+
+const COMPANY_POSITION = {
+  LAT: 37.5063252948439,
+  LNG: 127.0536146558608,
+};
